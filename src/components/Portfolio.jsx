565c685862c1aa4fa9225dc7db1e75e69ab1a7e9
yegoncollins <collinsyegon816@gmail.com>
2023-05-18 22:59:01 +0300

    lin

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,6 +4,10 @@ import Stream from "../assets/streamit.png"
 import Card from "../assets/card.png"
 
 const Portfolio = () => {
+    const newurl = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+      };
+    
   return (
     <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col items-center justify-center w-full h-full'>
@@ -15,19 +19,19 @@ const Portfolio = () => {
             <div className='shadow-md shadow-gray-600 rounded-lg'>
                 <img  className='rounded-md duration-200 hover:scale-105' src={Data} alt="" />
                 <div className='flex items-center justify-center'>
-                    <button className='w-1/2 py-1 px-4 m-4 duration-200 hover:scale-105'>Code</button>
+                    <button onClick={() => newurl('https://github.com/yegoncollins/data')} className='w-1/2 py-1 px-4 m-4 duration-200 hover:scale-105'>Code</button>
                 </div>
             </div>
             <div className='shadow-md shadow-gray-600 rounded-lg'>
                 <img  className='rounded-md duration-200 hover:scale-105' src={Stream} alt="" />
                 <div className='flex items-center justify-center'>
-                    <button className='w-1/2 py-1 px-4 m-4 duration-200 hover:scale-105'>Code</button>
+                    <button onClick={() => newurl('https://github.com/yegoncollins/streamit')} className='w-1/2 py-1 px-4 m-4 duration-200 hover:scale-105'>Code</button>
                 </div>
             </div>
             <div className='shadow-md shadow-gray-600 rounded-lg'>
                 <img  className='rounded-md duration-200 hover:scale-105' src={Card} alt="" />
                 <div className='flex items-center justify-center'>
-                    <button className='w-1/2 py-1 px-4 m-4 duration-200 hover:scale-105'>Code</button>
+                    <button onClick={() => newurl('https://github.com/yegoncollins/card-payment')} className='w-1/2 py-1 px-4 m-4 duration-200 hover:scale-105'>Code</button>
                 </div>
             </div>
             
