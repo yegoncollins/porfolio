380f7618509720c74d9761c945043d989a41932f
yegoncollins <collinsyegon816@gmail.com>
2023-05-21 15:54:12 +0300

    typed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import Typed from 'react-typed';
+
 import Hero from "../assets/bw.jpg"
 import {MdOutlineKeyboardArrowRight}from "react-icons/md"
 
 const Home = () => {
+
+    
+
+
   return (
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row relative'>
@@ -11,22 +15,12 @@ const Home = () => {
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
                     I Build & Design
                 </h2>
-                <Typed className=' md:text-3xl  md:pl-4 pl-2 text-[#feb786] text-xl py-2'
-            strings= {['Web Interfaces', 'Mobile Designs', 'Software Development']}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-            />
+                
             <p className='text-gray-500  max-w-md'>
                 I have experience building and designing software.<br/>
                 Currently, I love to work on web aplications using technologies like
             </p>
-            <Typed className=' md:text-xl  md:pl-4 pl-2 text-[#36e7d8] text-xl py-4'
-            strings= {['React', 'Tailwind', 'HTML','Javascript','CSS']}
-            typeSpeed={120}
-            backSpeed={140}
-            loop
-            />
+           
             <div>
                 <button className=' group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-300 to-blue-500 cursor-pointer'>
                     Portfolio
