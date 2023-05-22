6e7acae48a8b6f6235e70462f42e7fb2fdfb0b5a
yegoncollins <collinsyegon816@gmail.com>
2023-05-22 23:37:51 +0300

    update

--- src/components/Sociallinks.jsx
+++ src/components/Sociallinks.jsx
@@ -39,7 +39,7 @@ const Sociallinks = () => {
         </li>
         <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300  bg-gray-500'>
             <a 
-             href="/YEGON KIPLANGAT COLLINS CV.pdf"
+             href="/MR COLLINS KIPLANGAT.pdf"
              download={true}
 
              className='flex justify-between items-center w-full text-white'
